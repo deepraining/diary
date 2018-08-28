b04029a80ca705c73ab88c662f4fd4d41be17b25
senntyou <jiangjinbelief@163.com>
2018-08-28 10:57:23 +0800

    update

---
+++ screens/main.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Button, Text } from 'native-base';
+
+export default class Main extends Component {
+  render() {
+    return (
+      <Button success>
+        <Text> Success </Text>
+      </Button>
+    );
+  }
+}
