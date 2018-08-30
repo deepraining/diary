3da1145bfbb5a80fb03fbadb4ea20efe527feedd
senntyou <jiangjinbelief@163.com>
2018-08-30 10:27:58 +0800

    update

--- App.js
+++ App.js
@@ -1,13 +1,42 @@
-import React from 'react';
-import { StackNavigator } from 'react-navigation';
+import React, { Component } from 'react';
+import Expo, { Font } from 'expo';
+import { createStackNavigator } from 'react-navigation';
 import Main from './screens/main';
 
-const AppNavigator = StackNavigator({
+const AppNavigator = createStackNavigator({
   Main: { screen: Main },
 });
 
-export default class App extends React.Component {
+export default class App extends Component {
+  state = {
+    appIsReady: false,
+  };
+
+  componentWillMount() {
+    this.loadAssets();
+  }
+
+  async loadAssets() {
+    try {
+      await Font.loadAsync({
+        Roboto: require('native-base/Fonts/Roboto.ttf'),
+        Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+        Ionicons: require('@expo/vector-icons/fonts/Ionicons.ttf'),
+      });
+    } catch (e) {
+      console.log({ e });
+    } finally {
+      this.setState({ appIsReady: true });
+    }
+  }
+
   render() {
-    return <AppNavigator />;
+    const { appIsReady } = this.state;
+
+    if (appIsReady) {
+      return <AppNavigator />;
+    } else {
+      return <Expo.AppLoading />;
+    }
   }
 }
--- screens/main.js
+++ screens/main.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import { Button, Text } from 'native-base';
 
 export default class Main extends Component {
+  static navigationOptions = {
+    title: 'Diary by React-Native',
+  };
+
   render() {
     return (
       <Button success>
